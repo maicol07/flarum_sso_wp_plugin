ebad6ad3c331896d6625f487169332e8bb37e1c0
Maicol <maicolbattistini@live.it>
2020-10-10 15:10:45 +0200

    refactor: ♻️ Refactored public files

--- public/class-sso-flarum-public.php
+++ public/class-sso-flarum-public.php
@@ -49,10 +49,8 @@ class Flarum_SSO_Public {
 	 * @since    1.0.0
 	 */
 	public function __construct( string $plugin_name, string $version ) {
-
 		$this->plugin_name = $plugin_name;
 		$this->version     = $version;
-
 	}
 
 	/**
@@ -61,21 +59,7 @@ class Flarum_SSO_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles(): void {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Flarum_sso_plugin_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Flarum_sso_plugin_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/sso-flarum-public.css', array(), $this->version );
-
 	}
 
 	/**
@@ -84,21 +68,7 @@ class Flarum_SSO_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts(): void {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Flarum_sso_plugin_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Flarum_sso_plugin_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/sso-flarum-public.js', array( 'jquery' ), $this->version );
-
 	}
 
 }
--- public/partials/sso-flarum-public-display.php
+++ public/partials/sso-flarum-public-display.php
@@ -1,16 +1,1 @@
 <?php
-
-/**
- * Provide a public-facing view for the plugin
- *
- * This file is used to markup the public-facing aspects of the plugin.
- *
- * @link       https://maicol07.it
- * @since      1.0.0
- *
- * @package    sso-flarum
- * @subpackage sso-flarum/public/partials
- */
-?>
-
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
