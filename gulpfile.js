d76e569503e4abffa848e842140b9c6833375beb
Maicol <maicolbattistini@live.it>
2020-11-02 16:03:00 +0100

    build: Updated gulp script
    - Removed deprecated gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,64 @@
 const gulp = require('gulp');
-const clean = require('gulp-clean');
+const del = require('del');
 const zip = require('gulp-vinyl-zip').zip;
 const merge = require('merge-stream');
 
+/**
+ * Clean (deletes) the build and dist directories
+ *
+ * @returns {*}
+ */
 function clean_files() {
-	const build = gulp.src('build', {read: false, allowEmpty: true})
-		.pipe(clean());
-	const dist = gulp.src('dist', {read: false, allowEmpty: true})
-		.pipe(clean());
-
-	return merge(build, dist);
+	return del([
+		'build/**/*',
+		'/build/',
+		'dist/**/*',
+		'/dist/'
+	]);
 }
 
+/**
+ * Create the build directory with the required files for release
+ *
+ * @returns {*}
+ */
 function wp_build() {
-	const src = gulp.src('../src/**')
-		.pipe(gulp.dest('build/includes/src'));
-	const wp = gulp.src([
+	return gulp.src([
 		'**',
+		'index.php',
+		'!wp-cli.phar',
+		'!**/svn/',
+		'!**/svn/**/*',
 		'!**/node_modules/',
 		'!**/node_modules/**/*'
 	]).pipe(gulp.dest('build/'));
-
-	return merge(src, wp);
 }
 
+/**
+ * Create the plugin zip
+ *
+ * @returns {*}
+ */
 function wp_zip() {
 	return gulp.src('build/**/*')
 		.pipe(zip('sso-flarum.zip'))
 		.pipe(gulp.dest('dist'));
 }
 
-exports.default = gulp.series(clean_files, wp_build);
+/**
+ * Copy the build directory contents to svn (to push to SVN repo)
+ *
+ * @returns {*}
+ */
+function copy_svn() {
+	del([
+		'svn/**/*',
+		'!svn/.svn/**/*'
+	]);
+	return gulp.src('build/**/*').pipe(gulp.dest('svn'));
+}
+
+exports.default = gulp.series(clean_files, wp_build, copy_svn);
 exports.clean = clean_files;
 exports.zip = wp_zip;
+exports.copy_svn = copy_svn;
