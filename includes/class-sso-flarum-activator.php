527bb6088e8903ebaa89821c0429f41da6721f28
Maicol <maicolbattistini@live.it>
2021-05-23 15:42:33 +0200

    chore: 🔥 Removed composer auto installer

--- includes/class-sso-flarum-activator.php
+++ includes/class-sso-flarum-activator.php
@@ -37,27 +37,6 @@ class SSO_Flarum_Activator {
 				add_option( 'flarum_sso_plugin_version' );
 			}
 			update_option( 'flarum_sso_plugin_version', FLARUM_SSO_VERSION );
-
-			if ( get_option( 'flarum_sso_plugin_disable_composer_installer' ) ) {
-				return;
-			}
-			// Run composer install.
-			require 'composer/composer-install.php';
-			try {
-				$result = install_dependencies( ( defined( 'FLARUM_SSO_PATH' ) ? FLARUM_SSO_PATH : __DIR__ ) . '/includes/composer' );
-				if ( ! $result ) {
-					throw new Exception( $result );
-				}
-			} catch ( Exception $e ) {
-				/** @noinspection ForgottenDebugOutputInspection */
-				wp_die(
-					sprintf(
-						__( "Can't download dependencies. Please run the following command or download the plugin zip with all the dependencies from %s:<br><code>composer install --no-dev</code><br><br>Exception message: {$e->getMessage()}" ),
-						'<a href="https://github.com/maicol07/flarum_sso_wp_plugin/releases">' . __( 'Github releases' ) . '</a>'
-					)
-				);
-			}
 		}
 	}
-
 }
